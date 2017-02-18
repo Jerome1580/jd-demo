63385ce680243b0b429a905dc83a39982efdc8ea
Jerome1580 <raoju1580@qq.com>
2017-02-18 20:19:21 +0800

    代码优化
    1、通用base.js优化代码，减少全局变量
    2、完善验证用户注册登录判断逻辑

--- jsdetail/base.js
+++ jsdetail/base.js
@@ -3,62 +3,51 @@
  */
 
 
-//点击登录后，遮罩效果
-    var mask = document.getElementById("mask");
-    var Bomb_login = document.getElementById("Bomb_login");
-    var Bomb_register = document.getElementById("Bomb_register");
-    var close_mask = document.getElementById("close_mask_1");
-    var login = document.getElementById("login");
-    var register = document.getElementById("register");
+    //点击登录注册，遮罩效果
+    (function () {
+        $('#login').click(function () {
+            $('#mask,#Bomb_login').show();
+            $('body').css('overflow','hidden');  //禁止页面滚动
+        });
 
-    login.onclick= function(){
-        mask.style.display= "block";
-        Bomb_login.style.display= "block";
+        $('#register').click(function () {
+            $('#mask,#Bomb_register').show();
+            $('body').css('overflow','hidden');
+        });
 
-    };
+        $('#close_loginMask,#close_registerMask').click(function () {
+            $('#mask,#Bomb_login,#Bomb_register').hide();
+            $('body').css('overflow','auto');
+        });
 
-    register.onclick= function(){
-        mask.style.display= "block";
-        Bomb_register.style.display= "block";
+    })();
 
-    };
 
-    close_mask.onclick= function(){
-        mask.style.display= "none";
-        Bomb_login.style.display= "none";
-    };
+    //判断账户名注册合法性
+    (function () {
+        $('#login_name,#register_name').focus(function(){
+            $(this).css('borderColor', '#e43930');
+        }).blur(function () {
+            compareName($(this));
+        });
+    })();
 
-    close_mask_2.onclick= function(){
-        mask.style.display= "none";
-        Bomb_register.style.display= "none";
-    };
 
-   //判断用户注册合法性
+    function compareName(usename){
+        var nameTxt = usename.val();
+        var _tips =  usename.siblings('.tips');  //提示信息
+        var _tipsIcon =  _tips.find('i');
+        var _tipsTxt = _tips.find('span');
 
-     //1.获取用户输入值
-     //2.添加事件，进行判断，输出结果
-     $('#username').focus(function(){
-         //当input获取焦点时
-         $('#username').css('borderColor', '#e43930');
-
-     }).keyup(function () {
-
-     }).blur(function () {
-         var name = $('#username').val();
-         compareName(name);
-
-     });
-
-    function compareName(name){
-       if(name.length<5||name.length>12){
+       if(nameTxt.length<5 || nameTxt.length>12){
             //查找某一个元素的兄弟元素siblings，查找子元素find
-           $('#username').siblings('.tips').find('i').css('backgroundPosition', '-17px -100px');
-           $('#username').siblings('.tips').find('span').html('请输入5-12个字符');
+           _tipsIcon.css('backgroundPosition', '-17px -100px');
+           _tipsTxt.html('请输入5-12个字符');
         }
         else{
-           $('#username').siblings('.tips').find('i').css('backgroundPosition', '0 -117px');
-           $('#username').siblings('.tips').find('span').html('OK!');
-           $('#username').css('borderColor', '#f1f1f1');
+           _tipsIcon.css('backgroundPosition', '0 -117px');
+           _tipsTxt.html('OK!');
+           usename.css('borderColor', '#f1f1f1');
 
        }
     }
@@ -66,14 +55,24 @@
     //判断用户登录
 
 
-    //关闭广告
-    var jd_close =document.getElementById("jd-close");
-    var topbanner =document.getElementById("jd-top");
+    //关闭通栏广告
+    $('#jd_closeTopBanner').click(function () { $('#jd_topBanner').hide();});
 
+    //子频道页面，左侧slide全部品类折叠张开类目
+    $('#category').hover(function () {
+       $('#category>dd').show()
+     }, function () {
+       $('#category>dd').hide()
+    });
 
-    jd_close.onclick = function(){
-        topbanner.style.display = "none";
-    };
+    //购物车hover状态
+    $('#car').hover(function () {
+        $(this).addClass('hover');
+        $('#carDetail').show();
+    }, function () {
+        $(this).removeClass('hover');
+        $('#carDetail').hide();
+    });
 
     //搜索联想词
     $('#inputTxt').keyup(function () {
@@ -89,38 +88,9 @@
         $(document).click(function () {
             _box.hide();
         })
-
-
-});
-
-
-
-
-
-//折叠张开类目
-   $('#category').hover(function () {
-       $('#category>dd').show()
-   }, function () {
-       $('#category>dd').hide()
-   });
-
-
-
-
-
-    //购物车hover状态
-    $('#carDetail').hide();
-
-    $('#car').hover(function () {
-        $(this).addClass('hover');
-        $('#carDetail').show();
-    }, function () {
-        $(this).removeClass('hover');
-        $('#carDetail').hide();
     });
 
     //封装淘宝关键词搜索ajax请求数据
-
     function querySUG_TaoBao(_box,url){
 
         $.ajax({
@@ -150,13 +120,13 @@
 
         }
     }).done(function(){
-            chooseword(_box);//传递隐藏的盒子
+            chooseword(_box);//当用户选择联想词时
         })
             .fail(function (e) {
             console.log(e);
         })
 
-}
+};
     //选择关键词后隐藏联想词框
     function chooseword(_box){
         $('#queryKw>ul').on('click','li', function (e) {
